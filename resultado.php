8b447a234f48d7a1540510c0464ab2ede9911682
LuisJimenez <ljimenezt@kio.tech>
2024-07-17 11:05:22 -0600

    Se cambia el color a rojo y se enlaza pagina de resultado.php a la hoja de estilos

--- resultado.php
+++ resultado.php
@@ -9,3 +9,16 @@
 
     echo "<h1> Tu IMC es $IMC";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Resultado</title>
+</head>
+<body>
+    
+</body>
+</html>
